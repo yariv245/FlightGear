f8d3c12272d5b4fcbb2ff950005680dda6ca184b
Yariv Menachem <yariv220895@gmail.com>
2020-05-09 13:53:25 +0300

    yesysey

--- src/server_side/MySerialServer.java
+++ src/server_side/MySerialServer.java
@@ -1,17 +1,50 @@
 package server_side;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
 public class MySerialServer implements Server {
 
-	@Override
-	public void open(int port, ClientHandler ch) {
-		// TODO Auto-generated method stub
+	private volatile boolean stop;
 
+	@Override
+	public void open(int port, ClientHandler ch) throws IOException {
+		// TODO Auto-generated method stub
+		ServerSocket server = new ServerSocket(port);
+		server.setSoTimeout(1000);
+		while (!stop) {
+			try {
+				Socket aClient = server.accept();
+				try {
+					ch.handleClient(aClient.getInputStream(), aClient.getOutputStream());
+					aClient.getInputStream().close();
+					aClient.getOutputStream().close();
+					aClient.close();
+				} catch (IOException e) {
+					/* ... */}
+			} catch (SocketTimeoutException e) {
+				/* ... */}
+		}
+		server.close();
 	}
 
 	@Override
 	public void stop() {
 		// TODO Auto-generated method stub
-
+		stop = true;
 	}
-
+	
+	@Override
+	public void start(int port, ClientHandler ch) {
+		new Thread(()->{
+			try {
+				open(port,ch);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+			}
+		}).start();
+		
+	}
 }
--- src/server_side/Server.java
+++ src/server_side/Server.java
@@ -1,7 +1,10 @@
 package server_side;
 
+import java.io.IOException;
+
 public interface Server {
-	void open(int port,ClientHandler c); // Open server and wait for customers
+	void open(int port,ClientHandler c) throws IOException; // Open server and wait for customers
 	void stop(); // Close the server
+	void start(int port, ClientHandler ch);
 }
 	
